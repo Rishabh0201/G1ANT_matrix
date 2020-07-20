cb4fb6cbca2eff7862f1f9a0bb8b35a190677ef8
Rishabh0201 <57586304+Rishabh0201@users.noreply.github.com>
2020-07-20 23:24:58 +0530

    Add files via upload

---
+++ Swiggy_Addon/swiggycartCommand.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using G1ANT.Language;
+
+
+namespace G1ANT.Addon.Swiggy
+{
+    [Command(Name = "swiggy.cart", Tooltip = "This command open support page of swiggy ")]
+    public class cartCommand : Command
+    {
+        public cartCommand(AbstractScripter scripter) : base(scripter)
+        {
+
+        }
+        public class Arguments : SeleniumCommandArguments
+        {
+
+            [Argument(DefaultVariable = "timeoutselenium", Tooltip = "Specifies time in milliseconds for G1ANT.Robot to wait for the command to be executed")]
+            public override TimeSpanStructure Timeout { get; set; } = new TimeSpanStructure(SeleniumSettings.SeleniumTimeout);
+            [Argument(Tooltip = "By default, waits until the webpage fully loads")]
+            public BooleanStructure NoWait { get; set; } = new BooleanStructure(false);
+        }
+
+        public void Execute(Arguments arguments)
+        {
+            try
+            {
+
+                SeleniumManager.CurrentWrapper.Navigate("https://www.swiggy.com/checkout", arguments.Timeout.Value, arguments.NoWait.Value);
+
+            }
+
+            catch (Exception ex)
+            {
+                throw new ApplicationException($"Error occured while opening new selenium instance. Message: {ex.Message}", ex);
+            }
+        }
+    }
+
+
+}
---
+++ Swiggy_Addon/swiggyresturantownerCommand.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using G1ANT.Language;
+
+
+namespace G1ANT.Addon.Swiggy
+{
+    [Command(Name = "swiggy.resturantownerlogin", Tooltip = "This command is for resturant owners ")]
+    public class ownerswiggyloginCommand : Command
+    {
+        public ownerswiggyloginCommand(AbstractScripter scripter) : base(scripter)
+        {
+
+        }
+        public class Arguments : SeleniumCommandArguments
+        {
+            [Argument(Required = true, Tooltip = "Enter the email address here")]
+            public TextStructure email { get; set; }
+
+            [Argument(Required = true, Tooltip = "Enter the password  here")]
+            public TextStructure pass { get; set; }
+
+
+            [Argument(DefaultVariable = "timeoutselenium", Tooltip = "Specifies time in milliseconds for G1ANT.Robot to wait for the command to be executed")]
+            public override TimeSpanStructure Timeout { get; set; } = new TimeSpanStructure(SeleniumSettings.SeleniumTimeout);
+        }
+
+        public void Execute(Arguments arguments)
+        {
+            try
+            {
+                SeleniumWrapper wrapper = SeleniumManager.CreateWrapper(
+                        "chrome",
+                        "https://www.swiggyassist.in/Registration/Login.aspx",
+                        arguments.Timeout.Value,
+                        false,
+                        Scripter.Log,
+                        Scripter.Settings.UserDocsAddonFolder.FullName);
+                int wrapperId = wrapper.Id;
+                OnScriptEnd = () =>
+                {
+                    SeleniumManager.DisposeAllOpenedDrivers();
+                    SeleniumManager.RemoveWrapper(wrapperId);
+                    SeleniumManager.CleanUp();
+                };
+                arguments.Search.Value = "ctl00_ContentPlaceHolder1_ctl00_ctl01_Login1_UserName";
+                arguments.By.Value = "id";
+                SeleniumManager.CurrentWrapper.Click(arguments, arguments.Timeout.Value);
+                SeleniumManager.CurrentWrapper.TypeText(arguments.email.Value, arguments, arguments.Timeout.Value);
+                arguments.Search.Value = "ctl00_ContentPlaceHolder1_ctl00_ctl01_Login1_Password";
+                arguments.By.Value = "id";
+                SeleniumManager.CurrentWrapper.Click(arguments, arguments.Timeout.Value);
+                SeleniumManager.CurrentWrapper.TypeText(arguments.pass.Value, arguments, arguments.Timeout.Value);
+
+                SeleniumManager.CurrentWrapper.PressKey("enter", arguments, arguments.Timeout.Value);
+                
+            }
+
+            catch (Exception ex)
+            {
+                throw new ApplicationException($"Error occured while opening new selenium instance. Message: {ex.Message}", ex);
+            }
+        }
+    }
+
+
+}
