e0d885572780726d61416041704034f8055368e6
Rishabh0201 <57586304+Rishabh0201@users.noreply.github.com>
2020-07-21 23:18:50 +0530

    Add files via upload

---
+++ Telegram_Addon/telegramdownloadandroidCommand.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using G1ANT.Language;
+
+
+namespace G1ANT.Addon.Telegram
+{
+    [Command(Name = "telegram.androiddownload", Tooltip = "This command download telegram for android ")]
+    public class teledroiddownloadCommand : Command
+    {
+        public teledroiddownloadCommand(AbstractScripter scripter) : base(scripter)
+        {
+
+        }
+        public class Arguments : SeleniumCommandArguments
+        {
+
+            [Argument(Tooltip = "By default, waits until the webpage fully loads")]
+            public BooleanStructure NoWait { get; set; } = new BooleanStructure(false);
+
+
+            [Argument(DefaultVariable = "timeoutselenium", Tooltip = "Specifies time in milliseconds for G1ANT.Robot to wait for the command to be executed")]
+            public override TimeSpanStructure Timeout { get; set; } = new TimeSpanStructure(SeleniumSettings.SeleniumTimeout);
+        }
+
+        public void Execute(Arguments arguments)
+        {
+            try
+            {
+                SeleniumManager.CurrentWrapper.Navigate("https://play.google.com/store/apps/details?id=org.telegram.messenger", arguments.Timeout.Value, arguments.NoWait.Value);
+            }
+
+            catch (Exception ex)
+            {
+                throw new ApplicationException($"Error occured while opening new selenium instance. Message: {ex.Message}", ex);
+            }
+        }
+    }
+
+
+}
