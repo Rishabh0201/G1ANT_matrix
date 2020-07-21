b08c75a44f0845f5ba99c68d02e0412675269a03
Rishabh0201 <57586304+Rishabh0201@users.noreply.github.com>
2020-07-21 16:45:01 +0530

    Add files via upload

---
+++ IRCTC_Addon/irctcchartsCommand.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using G1ANT.Language;
+using System.Threading;
+
+
+namespace G1ANT.Addon.irctc
+{
+    [Command(Name = "irctc.charts", Tooltip = "This command check the charts on  irctc ")]
+    public class irctcchartsCommand : Command
+    {
+        public irctcchartsCommand(AbstractScripter scripter) : base(scripter)
+        {
+
+        }
+        public class Arguments : SeleniumCommandArguments
+        {
+
+            
+            [Argument(DefaultVariable = "timeoutselenium", Tooltip = "Specifies time in milliseconds for G1ANT.Robot to wait for the command to be executed")]
+            public override TimeSpanStructure Timeout { get; set; } = new TimeSpanStructure(SeleniumSettings.SeleniumTimeout);
+        }
+
+        public void Execute(Arguments arguments)
+        {
+            try
+            {
+
+                arguments.Search.Value = "/html/body/app-root/app-home/div[2]/div/app-main-page/div[1]/div/div[1]/div/div/div[1]/div/app-jp-input/div[5]/div[2]/a/label";
+                arguments.By.Value = "xpath";
+                SeleniumManager.CurrentWrapper.Click(arguments, arguments.Timeout.Value);
+                Thread.Sleep(3000);
+
+
+                
+
+
+
+
+            }
+
+            catch (Exception ex)
+            {
+                throw new ApplicationException($"Error occured while opening new selenium instance. Message: {ex.Message}", ex);
+            }
+        }
+    }
+
+
+}
---
+++ IRCTC_Addon/irctcecateringCommand.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using G1ANT.Language;
+using System.Threading;
+
+
+namespace G1ANT.Addon.irctc
+{
+    [Command(Name = "irctc.ecatering", Tooltip = "This command opnes e catering services on irctc ")]
+    public class irctcecateringCommand : Command
+    {
+        public irctcecateringCommand(AbstractScripter scripter) : base(scripter)
+        {
+
+        }
+        public class Arguments : SeleniumCommandArguments
+        {
+
+            [Argument(Required = true, Tooltip = "Enter the pnr number")]
+            public TextStructure pnr { get; set; }
+            [Argument(Tooltip = "By default, waits until the webpage fully loads")]
+            public BooleanStructure NoWait { get; set; } = new BooleanStructure(false);
+
+
+
+
+            [Argument(DefaultVariable = "timeoutselenium", Tooltip = "Specifies time in milliseconds for G1ANT.Robot to wait for the command to be executed")]
+            public override TimeSpanStructure Timeout { get; set; } = new TimeSpanStructure(SeleniumSettings.SeleniumTimeout);
+        }
+
+        public void Execute(Arguments arguments)
+        {
+            try
+            {
+
+                SeleniumManager.CurrentWrapper.Navigate("https://www.ecatering.irctc.co.in/", arguments.Timeout.Value, arguments.NoWait.Value);
+
+                arguments.Search.Value = "/html/body/div[1]/div/div/div/div/div/div";
+                arguments.By.Value = "xpath";
+                SeleniumManager.CurrentWrapper.Click(arguments, arguments.Timeout.Value);
+                
+                arguments.Search.Value = "/html/body/div[1]/div/section[2]/div[2]/input";
+                arguments.By.Value = "xpath";
+                SeleniumManager.CurrentWrapper.Click(arguments, arguments.Timeout.Value);
+                SeleniumManager.CurrentWrapper.TypeText(arguments.pnr.Value, arguments, arguments.Timeout.Value);
+                Thread.Sleep(1000);
+                SeleniumManager.CurrentWrapper.PressKey("enter", arguments, arguments.Timeout.Value);
+
+                
+
+
+
+
+            }
+
+            catch (Exception ex)
+            {
+                throw new ApplicationException($"Error occured while opening new selenium instance. Message: {ex.Message}", ex);
+            }
+        }
+    }
+
+
+}
