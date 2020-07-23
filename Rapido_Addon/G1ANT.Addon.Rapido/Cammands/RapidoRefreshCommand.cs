73ffde1f2af414ffbe32079903fd4b44c71d1aa6
Maneesh <56780894+maneesh06@users.noreply.github.com>
2020-07-23 21:03:05 +0530

    Add files via upload

---
+++ Rapido_Addon/G1ANT.Addon.Rapido/Cammands/RapidoRefreshCommand.cs
@@ -0,0 +1,31 @@
+﻿using G1ANT.Language;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G1ANT.Addon.Rapido
+{
+    [Command(Name = "rapido.refresh", Tooltip = "This command refreshes the current tab content in the web browser")]
+    public class RapidoRefreshCommand : Command
+    {
+        public class Arguments : CommandArguments
+        {
+        }
+        public RapidoRefreshCommand(AbstractScripter scripter) : base(scripter)
+        {
+        }
+        public void Execute(Arguments arguments)
+        {
+            try
+            {
+                SeleniumManager.CurrentWrapper.Refresh();
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException($"Error occured while refreshing selenium instance. Message: {ex.Message}", ex);
+            }
+        }
+    }
+}
