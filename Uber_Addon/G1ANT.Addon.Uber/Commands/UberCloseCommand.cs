fffff1ab32e258128c563b380c629c148457a8e7
Maneesh <56780894+maneesh06@users.noreply.github.com>
2020-07-24 12:12:41 +0530

    Add files via upload

---
+++ Uber_Addon/G1ANT.Addon.Uber/Commands/UberCloseCommand.cs
@@ -0,0 +1,32 @@
+﻿using G1ANT.Language;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G1ANT.Addon.Uber
+{
+    [Command(Name = "uber.close", Tooltip = "This command is use to closes a chrome web browser in which Uber is opened.")]
+    public class UberCloseCommand : Command
+    {
+        public class Arguments : CommandArguments
+        {
+        }
+        public UberCloseCommand(AbstractScripter scripter) : base(scripter)
+        {
+        }
+        public void Execute(Arguments arguments)
+        {
+            try
+            {
+                SeleniumManager.QuitCurrentWrapper();
+
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException($"Error occured while closing selenium instance. Message: {ex.Message}", ex);
+            }
+        }
+    }
+}
