a1a7680c18910b7ce1b2875900b9e98c24f2cffc
nayansarode23 <65156104+nayansarode23@users.noreply.github.com>
2020-08-11 00:26:59 +0530

    Add files via upload

---
+++ TwitterApp_Addon/G1ANT.Addon.TwitterApp/TwitterAppCloseCommand.cs
@@ -0,0 +1,25 @@
+﻿using G1ANT.Language;
+using OpenQA.Selenium.Remote;
+
+namespace G1ANT.Addon.TwitterApp
+{
+    [Command(Name = "twitterapp.close", Tooltip = "This command closes Twitter app")]
+    public class CloseCommand : Language.Command
+    {
+        public class Arguments : CommandArguments
+        {
+
+        }
+
+        public CloseCommand(AbstractScripter scripter) : base(scripter)
+        {
+
+        }
+
+        public void Execute(Arguments arguments)
+        {
+            var driver = OpenCommand.GetDriver();
+            driver.Quit();
+        }
+    }
+}
