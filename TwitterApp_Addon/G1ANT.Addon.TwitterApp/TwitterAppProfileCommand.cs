5beb412eadf18f14a6b2a04148607480a09e938d
nayansarode23 <65156104+nayansarode23@users.noreply.github.com>
2020-08-11 00:32:44 +0530

    Add files via upload

---
+++ TwitterApp_Addon/G1ANT.Addon.TwitterApp/TwitterAppProfileCommand.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using G1ANT.Language;
+using System.Threading;
+using OpenQA.Selenium.Appium.MultiTouch;
+using OpenQA.Selenium.Remote;
+
+namespace G1ANT.Addon.TwitterApp
+{
+    [Command(Name = "twitterapp.profile", Tooltip = "This command helps you to see your profile details on Twitter App")]
+    public class TwitterAppProfileCommand : Language.Command
+    {
+        public class Arguments : CommandArguments
+        {
+
+
+            [Argument(Required = false, Tooltip = "Provide name of the capaility")]
+            public TextStructure Search { get; set; } = new TextStructure("");
+
+            [Argument(Required = false, Tooltip = "Provide element ID")]
+            public TextStructure By { get; set; } = new TextStructure("id");
+        }
+
+        public TwitterAppProfileCommand(AbstractScripter scripter) : base(scripter)
+        {
+
+        }
+
+        public void Execute(Arguments arguments)
+        {
+
+            arguments.Search.Value = "Show navigation drawer";
+            arguments.By.Value = "accessibility id";
+            ElementHelper.GetElement(arguments.By.Value.ToLower(), arguments.Search.Value).Click();
+            Thread.Sleep(2000);
+            arguments.Search.Value = "com.twitter.android:id/fleetline_loading_holder";
+            arguments.By.Value = "id";
+            ElementHelper.GetElement(arguments.By.Value.ToLower(), arguments.Search.Value).Click();
+            Thread.Sleep(2000);
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
