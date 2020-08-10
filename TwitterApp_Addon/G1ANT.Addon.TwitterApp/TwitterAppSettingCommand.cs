8062423ac2d35f3cfbfee43a652ecd27d1a12e3a
nayansarode23 <65156104+nayansarode23@users.noreply.github.com>
2020-08-11 00:34:07 +0530

    Add files via upload

---
+++ TwitterApp_Addon/G1ANT.Addon.TwitterApp/TwitterAppSettingCommand.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using G1ANT.Language;
+using System.Threading;
+using OpenQA.Selenium.Appium.MultiTouch;
+using OpenQA.Selenium.Remote;
+
+namespace G1ANT.Addon.TwitterApp
+{
+    [Command(Name = "twitterapp.setting", Tooltip = "This command helps you to set setting of Twitter App")]
+    public class TwitterAppSettingCommand : Language.Command
+    {
+        public class Arguments : CommandArguments
+        {
+
+
+            [Argument(Required = false, Tooltip = "Provide name of the capaility")]
+            public TextStructure Search { get; set; } = new TextStructure("");
+
+            [Argument(Required = false, Tooltip = "Provide element ID")]
+            public TextStructure By { get; set; } = new TextStructure("id");
+        }
+
+        public TwitterAppSettingCommand(AbstractScripter scripter) : base(scripter)
+        {
+
+        }
+
+        public void Execute(Arguments arguments)
+        {
+            arguments.Search.Value = "Show navigation drawer";
+            arguments.By.Value = "accessibility id";
+            ElementHelper.GetElement(arguments.By.Value.ToLower(), arguments.Search.Value).Click();
+            Thread.Sleep(2000);
+            arguments.Search.Value = "com.twitter.android:id/empty_button_positive";
+            arguments.By.Value = "id";
+            ElementHelper.GetElement(arguments.By.Value.ToLower(), arguments.Search.Value).Click();
+            Thread.Sleep(2000);
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
