10ca545942e84a525b064ab1a6a7fb08d633e426
nayansarode23 <65156104+nayansarode23@users.noreply.github.com>
2020-08-11 22:44:31 +0530

    Add files via upload

---
+++ MediumApp_Addon/G1ANT.Addon.MediumApp/MediumAppLoginCommand.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using G1ANT.Language;
+using System.Threading;
+using OpenQA.Selenium.Appium.MultiTouch;
+using OpenQA.Selenium.Remote;
+
+namespace G1ANT.Addon.MediumApp
+{
+    [Command(Name = "mediumapp.login", Tooltip = "This command helps you to login your account in Medium App")]
+    public class MediumAppLoginCommand : Language.Command
+    {
+        public class Arguments : CommandArguments
+        {
+            [Argument(Required = true, Tooltip = "Type text here which you want to search.")]
+            public TextStructure Text { get; set; } = new TextStructure("");
+
+            [Argument(Required = false, Tooltip = "Provide name of the capaility")]
+            public TextStructure Search { get; set; } = new TextStructure("");
+
+            [Argument(Required = false, Tooltip = "Provide element ID")]
+            public TextStructure By { get; set; } = new TextStructure("id");
+        }
+
+        public MediumAppLoginCommand(AbstractScripter scripter) : base(scripter)
+        {
+
+        }
+
+        public void Execute(Arguments arguments)
+        {
+
+            arguments.Search.Value = "com.medium.reader:id/sign_in_account_question_button";
+            arguments.By.Value = "id";
+            ElementHelper.GetElement(arguments.By.Value.ToLower(), arguments.Search.Value).Click();
+            Thread.Sleep(2000);
+            arguments.Search.Value = "com.medium.reader:id/sign_in_email_button";
+            arguments.By.Value = "id";
+            ElementHelper.GetElement(arguments.By.Value.ToLower(), arguments.Search.Value).Click();
+            Thread.Sleep(2000);
+            arguments.Search.Value = "com.medium.reader:id/email_login_email_input";
+            arguments.By.Value = "id";
+            ElementHelper.GetElement(arguments.By.Value, arguments.Search.Value).SendKeys(arguments.Text.Value);
+            Thread.Sleep(100);
+            arguments.Search.Value = "com.medium.reader:id/email_login_button";
+            arguments.By.Value = "id";
+            ElementHelper.GetElement(arguments.By.Value.ToLower(), arguments.Search.Value).Click();
+            Thread.Sleep(2000);
+
+
+
+
+
+        }
+    }
+}
