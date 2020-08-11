a3bf97f582e723c11a8ba0d603a7d2e3c9b7accb
nayansarode23 <65156104+nayansarode23@users.noreply.github.com>
2020-08-11 16:48:43 +0530

    Add files via upload

---
+++ ZomatoApp_Addon/G1ANT.Addon.ZomatoApp/ZomatoAppLoginCommand.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using G1ANT.Language;
+using System.Threading;
+using OpenQA.Selenium.Appium.MultiTouch;
+using OpenQA.Selenium.Remote;
+
+namespace G1ANT.Addon.ZomatoApp
+{
+    [Command(Name = "zomatoapp.login", Tooltip = "This command helps you to login your account in Zomato App")]
+    public class ZomatoAppLoginCommand : Language.Command
+    {
+        public class Arguments : CommandArguments
+        {
+
+
+            [Argument(Required = false, Tooltip = "Provide name of the capaility")]
+            public TextStructure Search { get; set; } = new TextStructure("");
+
+            [Argument(Required = false, Tooltip = "Provide element ID")]
+            public TextStructure By { get; set; } = new TextStructure("id");
+        }
+
+        public ZomatoAppLoginCommand(AbstractScripter scripter) : base(scripter)
+        {
+
+        }
+
+        public void Execute(Arguments arguments)
+        {
+
+            arguments.Search.Value = "com.application.zomato:id/layout_signup";
+            arguments.By.Value = "id";
+            ElementHelper.GetElement(arguments.By.Value.ToLower(), arguments.Search.Value).Click();
+            Thread.Sleep(2000);
+            
+
+
+
+
+
+
+
+        }
+    }
+}
