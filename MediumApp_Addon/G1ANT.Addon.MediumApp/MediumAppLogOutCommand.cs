d5486de4f1c65f2b3d2df9ae610bdbbefb4b0e4e
nayansarode23 <65156104+nayansarode23@users.noreply.github.com>
2020-08-11 22:44:55 +0530

    Add files via upload

---
+++ MediumApp_Addon/G1ANT.Addon.MediumApp/MediumAppLogOutCommand.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using G1ANT.Language;
+using System.Threading;
+using OpenQA.Selenium.Appium.MultiTouch;
+using OpenQA.Selenium.Remote;
+
+namespace G1ANT.Addon.MediumApp
+{
+    [Command(Name = "mediumapp.logout", Tooltip = "This command helps you to logout your Medium App Account")]
+    public class MediumAppLogOutCommand : Language.Command
+    {
+        public class Arguments : CommandArguments
+        {
+            [Argument(Required = true, Tooltip = "Type text here which you want to search.")]
+            public TextStructure Text { get; set; } = new TextStructure("");
+
+            [Argument(Required = false, Tooltip = "Provide name of the capaility")]
+            public TextStructure Search { get; set; } = new TextStructure("");
+
+            [Argument(Required = false, Tooltip = "Provide element ID")]
+            public TextStructure By { get; set; } = new TextStructure("id");
+        }
+
+        public MediumAppLogOutCommand(AbstractScripter scripter) : base(scripter)
+        {
+
+        }
+
+        public void Execute(Arguments arguments)
+        {
+
+            arguments.Search.Value = "Open navigation drawer";
+            arguments.By.Value = "accessibilityid";
+            ElementHelper.GetElement(arguments.By.Value.ToLower(), arguments.Search.Value).Click();
+            Thread.Sleep(2000);
+            arguments.Search.Value = "com.medium.reader:id/nav_settings";
+            arguments.By.Value = "id";
+            ElementHelper.GetElement(arguments.By.Value.ToLower(), arguments.Search.Value).Click();
+            Thread.Sleep(2000);
+            arguments.Search.Value = "com.medium.reader:id/settings_sign_out";
+            arguments.By.Value = "id";
+            ElementHelper.GetElement(arguments.By.Value.ToLower(), arguments.Search.Value).Click();
+            Thread.Sleep(2000);
+
+
+
+
+
+
+
+        }
+    }
+}
