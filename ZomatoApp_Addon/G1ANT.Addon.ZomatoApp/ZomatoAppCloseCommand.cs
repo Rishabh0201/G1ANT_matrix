93c1d9ccbeb94c6b7f124a951431a536ea47f77a
nayansarode23 <65156104+nayansarode23@users.noreply.github.com>
2020-08-11 16:44:55 +0530

    Add files via upload

---
+++ ZomatoApp_Addon/G1ANT.Addon.ZomatoApp/ZomatoAppCloseCommand.cs
@@ -0,0 +1,25 @@
+﻿using G1ANT.Language;
+using OpenQA.Selenium.Remote;
+
+namespace G1ANT.Addon.ZomatoApp
+{
+    [Command(Name = "zomatoapp.close", Tooltip = "This command closes Zomato app")]
+    public class CloseCommand : Language.Command
+    {
+        public class Arguments : CommandArguments
+        {
+
+        }
+
+        public CloseCommand(AbstractScripter scripter) : base(scripter)
+        {
+
+        }
+
+        public void Execute(Arguments arguments)
+        {
+            var driver = OpenCommand.GetDriver();
+            driver.Quit();
+        }
+    }
+}
