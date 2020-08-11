dd2ae493a94db597acfd7dddfb3ace430a72d316
nayansarode23 <65156104+nayansarode23@users.noreply.github.com>
2020-08-11 16:45:40 +0530

    Add files via upload

---
+++ ZomatoApp_Addon/G1ANT.Addon.ZomatoApp/ZomatoAppCollectionCommand.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using G1ANT.Language;
+using System.Threading;
+using OpenQA.Selenium.Appium.MultiTouch;
+using OpenQA.Selenium.Remote;
+
+namespace G1ANT.Addon.ZomatoApp
+{
+    [Command(Name = "zomatoapp.collection", Tooltip = "This command helps you to see collection of food dishes on zomato App")]
+    public class ZomatoAppCollectionCommand : Language.Command
+    {
+        public class Arguments : CommandArguments
+        {
+
+
+            [Argument(Required = false, Tooltip = "Provide name of the capaility")]
+            public TextStructure Search { get; set; } = new TextStructure("");
+
+            [Argument(Required = false, Tooltip = "Provide element ID")]
+            public TextStructure By { get; set; } = new TextStructure("id");
+        }
+
+        public ZomatoAppCollectionCommand(AbstractScripter scripter) : base(scripter)
+        {
+
+        }
+
+        public void Execute(Arguments arguments)
+        {
+
+            arguments.Search.Value = "com.application.zomato:id/seeAllTV";
+            arguments.By.Value = "id";
+            ElementHelper.GetElement(arguments.By.Value.ToLower(), arguments.Search.Value).Click();
+            Thread.Sleep(2000);
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
