200f5d2f3bdfc079434bf6ab7e0e690aa7c0aa35
nayansarode23 <65156104+nayansarode23@users.noreply.github.com>
2020-08-11 22:45:46 +0530

    Add files via upload

---
+++ MediumApp_Addon/G1ANT.Addon.MediumApp/MediumAppProfileCommand.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using G1ANT.Language;
+using System.Threading;
+using OpenQA.Selenium.Appium.MultiTouch;
+using OpenQA.Selenium.Remote;
+
+namespace G1ANT.Addon.MediumApp
+{
+    [Command(Name = "mediumapp.profile", Tooltip = "This command helps you to see profile of Medium App Account")]
+    public class MediumAppProfileCommand : Language.Command
+    {
+        public class Arguments : CommandArguments
+        {
+
+
+            [Argument(Required = false, Tooltip = "Provide name of the capaility")]
+            public TextStructure Search { get; set; } = new TextStructure("");
+
+            [Argument(Required = false, Tooltip = "Provide element ID")]
+            public TextStructure By { get; set; } = new TextStructure("id");
+        }
+
+        public MediumAppProfileCommand(AbstractScripter scripter) : base(scripter)
+        {
+
+        }
+
+        public void Execute(Arguments arguments)
+        {
+            arguments.Search.Value = "Open navigation drawer";
+            arguments.By.Value = "accessibilityid";
+            ElementHelper.GetElement(arguments.By.Value.ToLower(), arguments.Search.Value).Click();
+            Thread.Sleep(2000);
+            arguments.Search.Value = "com.medium.reader:id/nav_your_profile";
+            arguments.By.Value = "xpath";
+            ElementHelper.GetElement(arguments.By.Value.ToLower(), arguments.Search.Value).Click();
+            Thread.Sleep(2000);
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
