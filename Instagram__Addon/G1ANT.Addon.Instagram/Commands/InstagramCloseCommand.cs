634b669f674a7f762f1cb4e8cef35e80ee8468fb
Maneesh <56780894+maneesh06@users.noreply.github.com>
2020-08-12 17:12:57 +0530

    Add files via upload

---
+++ Instagram__Addon/G1ANT.Addon.Instagram/Commands/InstagramCloseCommand.cs
@@ -0,0 +1,32 @@
+﻿using G1ANT.Language;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G1ANT.Addon.Instagram
+{
+    [Command(Name = "instagram.close", Tooltip = "This command is use to closes a web browser in which Instagram is opened")]
+    public class InstagramCloseCommand : Command
+    {
+        public class Arguments : CommandArguments
+        {
+        }
+        public InstagramCloseCommand(AbstractScripter scripter) : base(scripter)
+        {
+        }
+        public void Execute(Arguments arguments)
+        {
+            try
+            {
+                SeleniumManager.QuitCurrentWrapper();
+
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException($"Error occured while closing selenium instance. Message: {ex.Message}", ex);
+            }
+        }
+    }
+}
---
+++ Instagram__Addon/G1ANT.Addon.Instagram/Commands/InstagramRefreshCommand.cs
@@ -0,0 +1,31 @@
+﻿using G1ANT.Language;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G1ANT.Addon.Instagram
+{
+    [Command(Name = "instagram.refresh", Tooltip = "This command refreshes the current tab content in the web browser")]
+    public class InstagramRefreshCommand : Command
+    {
+        public class Arguments : CommandArguments
+        {
+        }
+        public InstagramRefreshCommand(AbstractScripter scripter) : base(scripter)
+        {
+        }
+        public void Execute(Arguments arguments)
+        {
+            try
+            {
+                SeleniumManager.CurrentWrapper.Refresh();
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException($"Error occured while refreshing selenium instance. Message: {ex.Message}", ex);
+            }
+        }
+    }
+}
