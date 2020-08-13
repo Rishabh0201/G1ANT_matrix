a2a12d5e5cc25a9b625544fdff14c3f75ebbbf9c
nayansarode23 <65156104+nayansarode23@users.noreply.github.com>
2020-08-13 16:41:29 +0530

    Add files via upload

---
+++ TumblrApp_Addon/G1ANT.Addon.TumblrApp/TumblrAppSettingCommand.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using G1ANT.Language;
+using System.Threading;
+using OpenQA.Selenium.Appium.MultiTouch;
+using OpenQA.Selenium.Remote;
+
+namespace G1ANT.Addon.TumblrApp
+{
+    [Command(Name = "tumblrapp.setting", Tooltip = "This command helps you to set setting of Medium App")]
+    public class TumblrAppSettingCommand : Language.Command
+    {
+        public class Arguments : CommandArguments
+        {
+            [Argument(Required = true, Tooltip = "Type text here which you want to search.")]
+            public TextStructure Text { get; set; } = new TextStructure("");
+
+            [Argument(Required = false, Tooltip = "Provide name of the capaility")]
+            public TextStructure Search { get; set; } = new TextStructure("");
+
+            [Argument(Required = false, Tooltip = "Provide element ID")]
+            public TextStructure By { get; set; } = new TextStructure("id");
+        }
+
+        public TumblrAppSettingCommand(AbstractScripter scripter) : base(scripter)
+        {
+
+        }
+
+        public void Execute(Arguments arguments)
+        {
+
+            arguments.Search.Value = "com.tumblr:id/topnav_account_button_img_active";
+            arguments.By.Value = "id";
+            ElementHelper.GetElement(arguments.By.Value.ToLower(), arguments.Search.Value).Click();
+            Thread.Sleep(2000);
+            arguments.Search.Value = "com.tumblr:id/action_blog_options";
+            arguments.By.Value = "id";
+            ElementHelper.GetElement(arguments.By.Value.ToLower(), arguments.Search.Value).Click();
+            Thread.Sleep(2000);
+            
+
+
+
+
+
+
+        }
+    }
+}
