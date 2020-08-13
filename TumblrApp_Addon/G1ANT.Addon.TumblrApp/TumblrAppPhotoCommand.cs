148e53cfd2e07489458295eea95a144bc144eade
nayansarode23 <65156104+nayansarode23@users.noreply.github.com>
2020-08-13 16:37:34 +0530

    Add files via upload

---
+++ TumblrApp_Addon/G1ANT.Addon.TumblrApp/TumblrAppPhotoCommand.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using G1ANT.Language;
+using System.Threading;
+using OpenQA.Selenium.Appium.MultiTouch;
+using OpenQA.Selenium.Remote;
+
+namespace G1ANT.Addon.TumblrApp
+{
+    [Command(Name = "tumblrapp.photo", Tooltip = "This command helps you to post photo in Tumblr App")]
+    public class TumblrAppPhotoCommand : Language.Command
+    {
+        public class Arguments : CommandArguments
+        {
+            [Argument(Required = true, Tooltip = "Type text here which you want to search.")]
+            public TextStructure Text { get; set; } = new TextStructure("");
+
+            [Argument(Required = false, Tooltip = "Provide name of the capaility")]
+            public TextStructure Search { get; set; } = new TextStructure("");
+
+            [Argument(Required = false, Tooltip = "Provide element ID")]
+            public TextStructure By { get; set; } = new TextStructure("id");
+        }
+
+        public TumblrAppPhotoCommand(AbstractScripter scripter) : base(scripter)
+        {
+
+        }
+
+        public void Execute(Arguments arguments)
+        {
+
+            arguments.Search.Value = "com.tumblr:id/post_image_btn";
+            arguments.By.Value = "id";
+            ElementHelper.GetElement(arguments.By.Value.ToLower(), arguments.Search.Value).Click();
+            Thread.Sleep(2000);
+           
+
+
+
+
+
+        }
+    }
+}
