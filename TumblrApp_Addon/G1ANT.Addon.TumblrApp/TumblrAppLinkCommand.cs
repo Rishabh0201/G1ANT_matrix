f9414ff27ed186f420c0556d00295e7e20a54e54
nayansarode23 <65156104+nayansarode23@users.noreply.github.com>
2020-08-13 16:35:44 +0530

    Add files via upload

---
+++ TumblrApp_Addon/G1ANT.Addon.TumblrApp/TumblrAppLinkCommand.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using G1ANT.Language;
+using System.Threading;
+using OpenQA.Selenium.Appium.MultiTouch;
+using OpenQA.Selenium.Remote;
+
+namespace G1ANT.Addon.TumblrApp
+{
+    [Command(Name = "tumblrapp.link", Tooltip = "This command helps you to post link in Tumblr App")]
+    public class TumblrAppLinkCommand : Language.Command
+    {
+        public class Arguments : CommandArguments
+        {
+            [Argument(Required = true, Tooltip = "Type text here which you want to search.")]
+            public TextStructure Text { get; set; } = new TextStructure("");
+
+            [Argument(Required = false, Tooltip = "Provide name of the capaility")]
+            public TextStructure Search { get; set; } = new TextStructure("");
+
+            [Argument(Required = false, Tooltip = "Provide element ID")]
+            public TextStructure By { get; set; } = new TextStructure("id");
+        }
+
+        public TumblrAppLinkCommand(AbstractScripter scripter) : base(scripter)
+        {
+
+        }
+
+        public void Execute(Arguments arguments)
+        {
+
+            arguments.Search.Value = "com.tumblr:id/post_link_block";
+            arguments.By.Value = "id";
+            ElementHelper.GetElement(arguments.By.Value.ToLower(), arguments.Search.Value).Click();
+            Thread.Sleep(2000);
+            
+
+
+
+
+
+        }
+    }
+}
---
+++ TumblrApp_Addon/G1ANT.Addon.TumblrApp/TumblrAppLoginCommand.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using G1ANT.Language;
+using System.Threading;
+using OpenQA.Selenium.Appium.MultiTouch;
+using OpenQA.Selenium.Remote;
+
+namespace G1ANT.Addon.TumblrApp
+{
+    [Command(Name = "tumblrapp.login", Tooltip = "This command helps you to login your account in Tumblr App")]
+    public class TumblrAppLoginCommand : Language.Command
+    {
+        public class Arguments : CommandArguments
+        {
+            [Argument(Required = true, Tooltip = "Type text here which you want to search.")]
+            public TextStructure Text { get; set; } = new TextStructure("");
+
+            [Argument(Required = false, Tooltip = "Provide name of the capaility")]
+            public TextStructure Search { get; set; } = new TextStructure("");
+
+            [Argument(Required = false, Tooltip = "Provide element ID")]
+            public TextStructure By { get; set; } = new TextStructure("id");
+        }
+
+        public TumblrAppLoginCommand(AbstractScripter scripter) : base(scripter)
+        {
+
+        }
+
+        public void Execute(Arguments arguments)
+        {
+
+            arguments.Search.Value = "com.tumblr:id/secondary_button";
+            arguments.By.Value = "id";
+            ElementHelper.GetElement(arguments.By.Value.ToLower(), arguments.Search.Value).Click();
+            Thread.Sleep(2000);            
+            arguments.Search.Value = "com.tumblr:id/email";
+            arguments.By.Value = "id";
+            ElementHelper.GetElement(arguments.By.Value, arguments.Search.Value).SendKeys(arguments.Text.Value);
+            Thread.Sleep(100);
+            arguments.Search.Value = "com.tumblr:id/primary_button";
+            arguments.By.Value = "id";
+            ElementHelper.GetElement(arguments.By.Value.ToLower(), arguments.Search.Value).Click();
+            Thread.Sleep(2000);
+            arguments.Search.Value = "com.tumblr:id/primary_button";
+            arguments.By.Value = "id";
+            ElementHelper.GetElement(arguments.By.Value.ToLower(), arguments.Search.Value).Click();
+            Thread.Sleep(2000);
+            
+
+
+
+
+        }
+    }
+}
