6496615aa54e817abcec98d96ca405e24776596e
nayansarode23 <65156104+nayansarode23@users.noreply.github.com>
2020-08-13 16:33:57 +0530

    Add files via upload

---
+++ TumblrApp_Addon/G1ANT.Addon.TumblrApp/TumblrAppCloseCommand.cs
@@ -0,0 +1,25 @@
+﻿using G1ANT.Language;
+using OpenQA.Selenium.Remote;
+
+namespace G1ANT.Addon.TumblrApp
+{
+    [Command(Name = "tumblrapp.close", Tooltip = "This command closes Tumblr app")]
+    public class CloseCommand : Language.Command
+    {
+        public class Arguments : CommandArguments
+        {
+
+        }
+
+        public CloseCommand(AbstractScripter scripter) : base(scripter)
+        {
+
+        }
+
+        public void Execute(Arguments arguments)
+        {
+            var driver = OpenCommand.GetDriver();
+            driver.Quit();
+        }
+    }
+}
