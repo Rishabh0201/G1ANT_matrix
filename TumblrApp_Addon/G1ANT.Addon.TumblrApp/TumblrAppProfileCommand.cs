e6b8aa5d8f478d15663b8f5d4be82d1918b1b8f8
nayansarode23 <65156104+nayansarode23@users.noreply.github.com>
2020-08-13 16:39:54 +0530

    Add files via upload

---
+++ TumblrApp_Addon/G1ANT.Addon.TumblrApp/TumblrAppProfileCommand.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using G1ANT.Language;
+using System.Threading;
+using OpenQA.Selenium.Appium.MultiTouch;
+using OpenQA.Selenium.Remote;
+
+namespace G1ANT.Addon.TumblrApp
+{
+    [Command(Name = "tumblrapp.profile", Tooltip = "This command helps you to see profile of Medium App Account")]
+    public class TumblrAppProfileCommand : Language.Command
+    {
+        public class Arguments : CommandArguments
+        {
+
+
+            [Argument(Required = false, Tooltip = "Provide name of the capaility")]
+            public TextStructure Search { get; set; } = new TextStructure("");
+
+            [Argument(Required = false, Tooltip = "Provide element ID")]
+            public TextStructure By { get; set; } = new TextStructure("id");
+        }
+
+        public TumblrAppProfileCommand(AbstractScripter scripter) : base(scripter)
+        {
+
+        }
+
+        public void Execute(Arguments arguments)
+        {
+            arguments.Search.Value = "com.tumblr:id/topnav_account_button_img_active";
+            arguments.By.Value = "id";
+            ElementHelper.GetElement(arguments.By.Value.ToLower(), arguments.Search.Value).Click();
+            Thread.Sleep(2000);
+            
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
