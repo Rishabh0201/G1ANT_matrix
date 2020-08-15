85f9c93fd6695d2368cc41185dd52704255df808
nayansarode23 <65156104+nayansarode23@users.noreply.github.com>
2020-08-15 18:03:10 +0530

    Add files via upload

---
+++ ZoomApp_Addon/G1ANT.Addon.ZoomApp/ZoomAppLoginCommand.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using G1ANT.Language;
+using System.Threading;
+using OpenQA.Selenium.Appium.MultiTouch;
+using OpenQA.Selenium.Remote;
+
+namespace G1ANT.Addon.ZoomApp
+{
+    [Command(Name = "zoomapp.login", Tooltip = "This command helps you to login your account in Zoom App")]
+    public class ZoomAppLoginCommand : Language.Command
+    {
+        public class Arguments : CommandArguments
+        {
+            [Argument(Required = true, Tooltip = "Type text here which you want to search.")]
+            public TextStructure Text { get; set; } = new TextStructure("");
+
+            [Argument(Required = false, Tooltip = "Provide name of the capaility")]
+            public TextStructure Search { get; set; } = new TextStructure("");
+
+            [Argument(Required = false, Tooltip = "Provide element ID")]
+            public TextStructure By { get; set; } = new TextStructure("id");
+        }
+
+        public ZoomAppLoginCommand(AbstractScripter scripter) : base(scripter)
+        {
+
+        }
+
+        public void Execute(Arguments arguments)
+        {
+
+            arguments.Search.Value = "us.zoom.videomeetings:id/btnLogin";
+            arguments.By.Value = "id";
+            ElementHelper.GetElement(arguments.By.Value.ToLower(), arguments.Search.Value).Click();
+            Thread.Sleep(2000);            
+
+
+
+
+
+        }
+    }
+}
