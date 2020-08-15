2860d585e4a2acf63c2aa6f7bb0922b1b13fadcb
nayansarode23 <65156104+nayansarode23@users.noreply.github.com>
2020-08-15 18:04:17 +0530

    Add files via upload

---
+++ ZoomApp_Addon/G1ANT.Addon.ZoomApp/ZoomAppProfileCommand.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using G1ANT.Language;
+using System.Threading;
+using OpenQA.Selenium.Appium.MultiTouch;
+using OpenQA.Selenium.Remote;
+
+namespace G1ANT.Addon.ZoomApp
+{
+    [Command(Name = "zoomapp.profile", Tooltip = "This command helps you to see profile of Zoom App Account")]
+    public class ZoomAppProfileCommand : Language.Command
+    {
+        public class Arguments : CommandArguments
+        {
+
+
+            [Argument(Required = false, Tooltip = "Provide name of the capaility")]
+            public TextStructure Search { get; set; } = new TextStructure("");
+
+            [Argument(Required = false, Tooltip = "Provide element ID")]
+            public TextStructure By { get; set; } = new TextStructure("id");
+        }
+
+        public ZoomAppProfileCommand(AbstractScripter scripter) : base(scripter)
+        {
+
+        }
+
+        public void Execute(Arguments arguments)
+        {
+            arguments.Search.Value = "Settings tab.";
+            arguments.By.Value = "accessibilityid";
+            ElementHelper.GetElement(arguments.By.Value.ToLower(), arguments.Search.Value).Click();
+            Thread.Sleep(2000);
+            arguments.Search.Value = "us.zoom.videomeetings:id/optionMMProfile";
+            arguments.By.Value = "id";
+            ElementHelper.GetElement(arguments.By.Value.ToLower(), arguments.Search.Value).Click();
+            Thread.Sleep(2000);
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
