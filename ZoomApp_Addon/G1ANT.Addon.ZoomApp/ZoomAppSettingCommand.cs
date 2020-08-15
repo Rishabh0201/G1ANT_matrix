d9592952cd4dc2ad015bf74ab81bd7185912801a
nayansarode23 <65156104+nayansarode23@users.noreply.github.com>
2020-08-15 18:05:05 +0530

    Add files via upload

---
+++ ZoomApp_Addon/G1ANT.Addon.ZoomApp/ZoomAppSettingCommand.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using G1ANT.Language;
+using System.Threading;
+using OpenQA.Selenium.Appium.MultiTouch;
+using OpenQA.Selenium.Remote;
+
+namespace G1ANT.Addon.ZoomApp
+{
+    [Command(Name = "zoomapp.setting", Tooltip = "This command helps you to set setting of Zoom App")]
+    public class ZoomAppSettingCommand : Language.Command
+    {
+        public class Arguments : CommandArguments
+        {
+            [Argument(Required = true, Tooltip = "Type text here which you want to search.")]
+            public TextStructure Text { get; set; } = new TextStructure("");
+
+            [Argument(Required = false, Tooltip = "Provide name of the capaility")]
+            public TextStructure Search { get; set; } = new TextStructure("");
+
+            [Argument(Required = false, Tooltip = "Provide element ID")]
+            public TextStructure By { get; set; } = new TextStructure("id");
+        }
+
+        public ZoomAppSettingCommand(AbstractScripter scripter) : base(scripter)
+        {
+
+        }
+
+        public void Execute(Arguments arguments)
+        {
+
+            arguments.Search.Value = "Settings tab.";
+            arguments.By.Value = "accessibilityid";
+            ElementHelper.GetElement(arguments.By.Value.ToLower(), arguments.Search.Value).Click();
+            Thread.Sleep(2000);
+           
+            
+
+
+
+
+
+
+        }
+    }
+}
