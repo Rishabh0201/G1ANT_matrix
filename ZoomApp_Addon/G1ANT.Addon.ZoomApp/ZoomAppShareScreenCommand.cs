5ab77af09a8f22543fc21875fdf6295aecaf1de1
nayansarode23 <65156104+nayansarode23@users.noreply.github.com>
2020-08-15 18:05:35 +0530

    Add files via upload

---
+++ ZoomApp_Addon/G1ANT.Addon.ZoomApp/ZoomAppShareScreenCommand.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using G1ANT.Language;
+using System.Threading;
+using OpenQA.Selenium.Appium.MultiTouch;
+using OpenQA.Selenium.Remote;
+
+namespace G1ANT.Addon.ZoomApp
+{
+    [Command(Name = "zoomapp.sharescreen", Tooltip = "This command helps you to share your mobile screen in Zoom App")]
+    public class ZoomAppShareScreenCommand : Language.Command
+    {
+        public class Arguments : CommandArguments
+        {
+            [Argument(Required = true, Tooltip = "Type text here which you want to search.")]
+            public TextStructure Text { get; set; } = new TextStructure("");
+
+            [Argument(Required = false, Tooltip = "Provide name of the capaility")]
+            public TextStructure Search { get; set; } = new TextStructure("");
+
+            [Argument(Required = false, Tooltip = "Provide element ID")]
+            public TextStructure By { get; set; } = new TextStructure("id");
+        }
+
+        public ZoomAppShareScreenCommand(AbstractScripter scripter) : base(scripter)
+        {
+
+        }
+
+        public void Execute(Arguments arguments)
+        {
+
+            arguments.Search.Value = "/html/body/div[1]/div/div[2]/div[1]/div/div/div/div/div/div[44]/div";
+            arguments.By.Value = "xpath";
+            ElementHelper.GetElement(arguments.By.Value.ToLower(), arguments.Search.Value).Click();
+            Thread.Sleep(2000);            
+
+
+
+
+
+        }
+    }
+}
