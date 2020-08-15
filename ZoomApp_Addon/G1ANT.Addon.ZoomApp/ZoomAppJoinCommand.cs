503412ef1206b1235ee9779bf912dae70cdfddc9
nayansarode23 <65156104+nayansarode23@users.noreply.github.com>
2020-08-15 18:02:19 +0530

    Add files via upload

---
+++ ZoomApp_Addon/G1ANT.Addon.ZoomApp/ZoomAppJoinCommand.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using G1ANT.Language;
+using System.Threading;
+using OpenQA.Selenium.Appium.MultiTouch;
+using OpenQA.Selenium.Remote;
+
+namespace G1ANT.Addon.ZoomApp
+{
+    [Command(Name = "zoomapp.join", Tooltip = "This command helps you to join meeting in Zoom App")]
+    public class ZoomAppJoinCommand : Language.Command
+    {
+        public class Arguments : CommandArguments
+        {
+            [Argument(Required = true, Tooltip = "Type text here which you want to search.")]
+            public TextStructure Text { get; set; } = new TextStructure("");
+
+            [Argument(Required = false, Tooltip = "Provide name of the capaility")]
+            public TextStructure Search { get; set; } = new TextStructure("");
+
+            [Argument(Required = false, Tooltip = "Provide element ID")]
+            public TextStructure By { get; set; } = new TextStructure("id");
+        }
+
+        public ZoomAppJoinCommand(AbstractScripter scripter) : base(scripter)
+        {
+
+        }
+
+        public void Execute(Arguments arguments)
+        {
+
+            arguments.Search.Value = "us.zoom.videomeetings:id/btnJoinConf";
+            arguments.By.Value = "id";
+            ElementHelper.GetElement(arguments.By.Value.ToLower(), arguments.Search.Value).Click();
+            Thread.Sleep(2000);           
+            arguments.Search.Value = "us.zoom.videomeetings:id/edtConfNumber";
+            arguments.By.Value = "id";
+            ElementHelper.GetElement(arguments.By.Value, arguments.Search.Value).SendKeys(arguments.Text.Value);
+            Thread.Sleep(100);
+            arguments.Search.Value = "us.zoom.videomeetings:id/btnJoin";
+            arguments.By.Value = "id";
+            ElementHelper.GetElement(arguments.By.Value.ToLower(), arguments.Search.Value).Click();
+            Thread.Sleep(2000);
+
+
+
+
+
+        }
+    }
+}
