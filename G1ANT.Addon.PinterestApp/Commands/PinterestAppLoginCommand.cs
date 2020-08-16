da538e49b7c82429dc7c9f80fda14a48fceb0e6d
suniladsul26 <67588919+suniladsul26@users.noreply.github.com>
2020-08-16 22:33:40 +0530

    Add files via upload

---
+++ G1ANT.Addon.PinterestApp/Commands/PinterestAppLoginCommand.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using G1ANT.Language;
+using System.Threading;
+using OpenQA.Selenium.Appium.MultiTouch;
+using OpenQA.Selenium.Remote;
+
+namespace G1ANT.Addon.PinterestApp
+{
+    [Command(Name = "pinterestapp.login", Tooltip = "This command login in the pinterest app.")]
+    public class pinterestAppLoginCommand : Language.Command
+    {
+        public class Arguments : CommandArguments
+        {
+            [Argument(Required = true, Tooltip = "Give your Email Id here.")]
+            public TextStructure Email { get; set; } = new TextStructure("");
+            [Argument(Required = true, Tooltip = "Give your Password here.")]
+            public TextStructure Pass { get; set; } = new TextStructure("");
+
+            [Argument(Required = false, Tooltip = "Provide name of the capaility")]
+            public TextStructure Search { get; set; } = new TextStructure("");
+
+            [Argument(Required = false, Tooltip = "Provide element ID")]
+            public TextStructure By { get; set; } = new TextStructure("id");
+        }
+
+        public pinterestAppLoginCommand(AbstractScripter scripter) : base(scripter)
+        {
+
+        }
+
+        public void Execute(Arguments arguments)
+        {
+            arguments.Search.Value = "Login";
+            arguments.By.Value = "accessibilityid";
+            var by = arguments.By.Value.ToLower();
+            ElementHelper.GetElement(by, arguments.Search.Value).Click();
+            Thread.Sleep(2000);
+
+            arguments.Search.Value = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout[2]/android.widget.LinearLayout/android.widget.ScrollView/android.widget.LinearLayout/android.widget.TextView[3]";
+            arguments.By.Value = "xpath";
+            ElementHelper.GetElement(arguments.By.Value.ToLower(), arguments.Search.Value).Click();
+            Thread.Sleep(1000);
+            ElementHelper.GetElement(arguments.By.Value.ToLower(), arguments.Search.Value).SendKeys(arguments.Email.Value);
+
+            arguments.Search.Value = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.support.v7.widget.RecyclerView/android.widget.LinearLayout[1]/android.widget.LinearLayout";
+            arguments.By.Value = "xpath";
+            ElementHelper.GetElement(arguments.By.Value.ToLower(), arguments.Search.Value).Click();
+            Thread.Sleep(1000);
+            ElementHelper.GetElement(arguments.By.Value, arguments.Search.Value).SendKeys(arguments.Pass.Value);
+            arguments.Search.Value = "...";
+            arguments.By.Value = "id";
+            ElementHelper.GetElement(arguments.By.Value.ToLower(), arguments.Search.Value).Click();
+            Thread.Sleep(8000);
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ G1ANT.Addon.PinterestApp/Commands/PinterestAppLogoutCommand.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using G1ANT.Language;
+using System.Threading;
+using OpenQA.Selenium.Appium.MultiTouch;
+using OpenQA.Selenium.Remote;
+
+namespace G1ANT.Addon.PinterestApp
+{
+    [Command(Name = "pinterestapp.logout", Tooltip = "This command logout from pinterest app")]
+    public class PinterestAppLogoutCommand : Language.Command
+    {
+        public class Arguments : CommandArguments
+        {
+
+
+            [Argument(Required = false, Tooltip = "Provide name of the capaility")]
+            public TextStructure Search { get; set; } = new TextStructure("");
+
+            [Argument(Required = false, Tooltip = "Provide element ID")]
+            public TextStructure By { get; set; } = new TextStructure("id");
+        }
+
+        public PinterestAppLogoutCommand(AbstractScripter scripter) : base(scripter)
+        {
+
+        }
+
+        public void Execute(Arguments arguments)
+        {
+
+            arguments.Search.Value = "...";
+            arguments.By.Value = "id";
+            ElementHelper.GetElement(arguments.By.Value.ToLower(), arguments.Search.Value).Click();
+            Thread.Sleep(100);
+            arguments.Search.Value = "...";
+            arguments.By.Value = "xpath";
+            ElementHelper.GetElement(arguments.By.Value.ToLower(), arguments.Search.Value).Click();
+            Thread.Sleep(200);
+            arguments.Search.Value = "//android.widget.ImageView[@content-desc=Settings]";
+            arguments.By.Value = "xpath";
+            ElementHelper.GetElement(arguments.By.Value.ToLower(), arguments.Search.Value).Click();
+            Thread.Sleep(500);
+            arguments.Search.Value = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.view.ViewGroup/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.LinearLayout/androidx.recyclerview.widget.RecyclerView/android.widget.LinearLayout[8]/android.widget.RelativeLayout/android.widget.TextView";
+            arguments.By.Value = "xpath";
+            ElementHelper.GetElement(arguments.By.Value.ToLower(), arguments.Search.Value).Click();
+            Thread.Sleep(500);
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
